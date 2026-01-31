3bba3464daa2b785cd4bdd59f31e17df26d72f68
mushfiq-isrt <m.r.tahmid31@isrt.ac.bd>
2026-01-31 13:34:58 +0600

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,14 @@
+# print
+print("Hello,World")
+
+
+# taking inputs
+#print("What's your name?")
+#input()
+#print("Hello")
+
+# making it more beautiful
+    # ask user for their name
+name = input("What is your name? ")
+print("Hello")
+print(name)
