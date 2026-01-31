77dc119d091332632c47d73ca0f200a6e263de98
mushfiq-isrt <m.r.tahmid31@isrt.ac.bd>
2026-01-31 15:07:01 +0600

    6th commit

--- hello.py
+++ hello.py
@@ -23,5 +23,14 @@ comment
 # use of concatination
 name = input("What is your name? ")
 print("Hello, " + name) # notice there's a space after hello,
+
+# understanding print fucntion
+name = input("What's your name? ")
+print("Hello, ", end="")
+print(name)
+
+# how do you quote in python then?
+print('hello,"friend"')
+
 """
 
